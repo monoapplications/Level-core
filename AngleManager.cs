1369cef0036ef42a504431e7b5d4d096ce94bc86
monoapplications <42820525+monoapplications@users.noreply.github.com>
2024-09-27 04:36:41 +0000

    Level-core ver1.0

---
+++ AngleManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AngleManager : MonoBehaviour
+{
+    public Text angleText;  // 表示するテキスト
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 加速度を取得
+        Vector3 acceleration = Input.acceleration;
+
+        // 傾きの方向
+        float gravityX = acceleration.x;
+        float gravityY = acceleration.y;
+        float gravityZ = acceleration.z;
+
+        // 矢印の方向
+        float arrow  = Mathf.Sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y);
+        float arrowX = acceleration.x / arrow;
+        float arrowY = acceleration.y / arrow;
+
+        // 傾き角度：θ
+        float gravity = Mathf.Sqrt(gravityX*gravityX + gravityY*gravityY + gravityZ*gravityZ);
+        float theta = Mathf.Acos(-gravityZ/gravity) * Mathf.Rad2Deg;
+
+        // UIに表示
+        angleText.text = "矢印X: " + arrowX.ToString("F1") + "\n" +
+                         "矢印Y: " + arrowY.ToString("F1") + "\n" +
+                         "角度 : " + theta.ToString("F1")  + "度";
+    }
+}
